bbf76684be1e40012c315b08241e4fe1e1286d8c
jalinei <jalinei@users.noreply.github.com>
2025-01-17 14:35:04 +0000

    Deployed f9b5eaf9 to 1.0.0 with MkDocs 1.6.1 and mike 2.1.3

--- 1.0.0/javascripts/version_selector.js
+++ 1.0.0/javascripts/version_selector.js
@@ -1,10 +1,17 @@
-document.addEventListener('DOMContentLoaded', function() {
+document$.subscribe(function() {
+    // Example: Initialize a third-party library or custom logic
+    console.log("Page fully loaded. Initializing custom JavaScript.");
+    
+    // Add your custom logic here
     var versionDropdown = document.getElementById('version-select');
-    versionDropdown.addEventListener('change', function() {
-        var selectedVersion = versionDropdown.value;
-        var currentPath = window.location.pathname.split('/');
-        currentPath.pop();  // Remove current file
-        currentPath.push(selectedVersion + '.html');
-        window.location.pathname = currentPath.join('/');
-    });
+    if (versionDropdown) {
+        versionDropdown.addEventListener('change', function() {
+            console.log("callback triggered version changed to ${versionDropdown.value});
+            var selectedVersion = versionDropdown.value;
+            var currentPath = window.location.pathname.split('/');
+            currentPath.pop();  // Remove current file
+            currentPath.push(selectedVersion + '.html');
+            window.location.pathname = currentPath.join('/');
+        });
+    }
 });
