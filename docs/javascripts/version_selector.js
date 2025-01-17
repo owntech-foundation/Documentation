0d15e040b3693146b72cd0c7eed0d81153971569
Jean Alinei <jean.alinei@owntech.org>
2025-01-17 16:15:15 +0100

    Update version_selector.js

--- docs/javascripts/version_selector.js
+++ docs/javascripts/version_selector.js
@@ -1,15 +1,24 @@
 document$.subscribe(function() {
-    // Example: Initialize a third-party library or custom logic
-    console.log("Page fully loaded. Initializing custom JavaScript.");
-    
-    // Add your custom logic here
+
+    // If there is a version selector on the page, 
+    // add a callback to change URL according to the selector
     var versionDropdown = document.getElementById('version-select');
     if (versionDropdown) {
         versionDropdown.addEventListener('change', function() {
+            console.log(`callback triggered version changed to ${versionDropdown.value}`);
             var selectedVersion = versionDropdown.value;
-            var currentPath = window.location.pathname.split('/');
-            currentPath.pop();  // Remove current file
+            
+            // Normalize the URL path by trimming trailing slashes
+            var currentPath = window.location.pathname;
+            if (currentPath.endsWith('/')) {
+                currentPath = currentPath.slice(0, -1);  // Remove the trailing slash
+            }
+            
+            currentPath = currentPath.split('/');
+            currentPath.pop();  // Remove the current file
             currentPath.push(selectedVersion + '.html');
+            
+            // Reconstruct the new path and navigate
             window.location.pathname = currentPath.join('/');
         });
     }
