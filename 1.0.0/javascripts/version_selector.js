7e6ecf75766031b8378e6d58445ed5c1b94aae29
jalinei <jalinei@users.noreply.github.com>
2025-01-17 15:48:59 +0000

    Deployed 5f7769fb to 1.0.0 with MkDocs 1.6.1 and mike 2.1.3

--- 1.0.0/javascripts/version_selector.js
+++ 1.0.0/javascripts/version_selector.js
@@ -2,27 +2,29 @@ document$.subscribe(function() {
 
     var versionDropdown = document.getElementById('version-select');
     if (versionDropdown) {
+        // Set the dropdown to match the current URL's version on page load
+        var currentPath = window.location.pathname.split('/');
+        if (currentPath[0] === "") {
+            currentPath.shift(); // Remove the leading empty element if present
+        }
+        var currentVersion = currentPath[currentPath.length - 1]; // Last segment of the path
+        versionDropdown.value = currentVersion;
+
         versionDropdown.addEventListener('change', function() {
             console.log(`callback triggered version changed to ${versionDropdown.value}`);
             var selectedVersion = versionDropdown.value;
 
-            var currentPath = window.location.pathname;
             if (currentPath.endsWith('/')) {
                 currentPath = currentPath.slice(0, -1);
             }
 
-            currentPath = currentPath.split('/');
-            
-            // Remove the first empty element if it exists
-            if (currentPath[0] === "") {
-                currentPath.shift();
-            }
-            
             currentPath.pop(); // Remove the current file
             currentPath.push(selectedVersion);
 
             // Reconstruct the new path
             var newPath = '/' + currentPath.join('/');
+            
+            console.log(`Navigating to new path: ${newPath}`);
             window.location.pathname = newPath;
         });
     }
