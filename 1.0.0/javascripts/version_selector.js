8fec33b3c6f27fc9abbc1195e90612813041b294
jalinei <jalinei@users.noreply.github.com>
2025-01-17 15:36:12 +0000

    Deployed 518e7578 to 1.0.0 with MkDocs 1.6.1 and mike 2.1.3

--- 1.0.0/javascripts/version_selector.js
+++ 1.0.0/javascripts/version_selector.js
@@ -1,25 +1,29 @@
 document$.subscribe(function() {
 
-    // If there is a version selector on the page, 
-    // add a callback to change URL according to the selector
     var versionDropdown = document.getElementById('version-select');
     if (versionDropdown) {
         versionDropdown.addEventListener('change', function() {
             console.log(`callback triggered version changed to ${versionDropdown.value}`);
             var selectedVersion = versionDropdown.value;
-            
-            // Normalize the URL path by trimming trailing slashes
+
             var currentPath = window.location.pathname;
             if (currentPath.endsWith('/')) {
-                currentPath = currentPath.slice(0, -1);  // Remove the trailing slash
+                currentPath = currentPath.slice(0, -1);
+            }
+
+            currentPath = currentPath.split('/');
+            
+            // Remove the first empty element if it exists
+            if (currentPath[0] === "") {
+                currentPath.shift();
             }
             
-            currentPath = currentPath.split('/');
-            currentPath.pop();  // Remove the current file
-            currentPath.push(selectedVersion + '.html');
-            
-            // Reconstruct the new path and navigate
-            window.location.pathname = currentPath.join('/');
+            currentPath.pop(); // Remove the current file
+            currentPath.push(selectedVersion);
+
+            // Reconstruct the new path
+            var newPath = '/' + currentPath.join('/');
+            window.location.pathname = newPath;
         });
     }
 });
