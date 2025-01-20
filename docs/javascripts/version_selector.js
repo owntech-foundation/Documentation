db0aeb42b62e470e3a9e13d83a3fc2c2fa4c5a54
Jean Alinei <jean.alinei@owntech.org>
2025-01-20 11:00:48 +0100

    Update version_selector.js

--- docs/javascripts/version_selector.js
+++ docs/javascripts/version_selector.js
@@ -1,31 +1,55 @@
-document$.subscribe(function() {
+import { requestJSON } from "~/browser";
+import { configuration } from "~/_";
+import { Version } from "~/templates";
 
-    var versionDropdown = document.getElementById('version-select');
-    if (versionDropdown) {
-        // Set the dropdown to match the current URL's version on page load
-        var currentPath = window.location.pathname.split('/');
-        if (currentPath[0] === "") {
-            currentPath.shift(); // Remove the leading empty element if present
-        }
-        var currentVersion = currentPath[currentPath.length - 2]; 
-        versionDropdown.value = currentVersion;
+async function getCurrentVersion() {
+  const config = configuration();
+  const versions = await requestJSON<Version[]>(new URL("../versions.json", config.base)).toPromise();
 
-        versionDropdown.addEventListener('change', function() {
-            console.log(`callback triggered version changed to ${versionDropdown.value}`);
-            var selectedVersion = versionDropdown.value;
+  // Extract current version from the base URL
+  const [, currentVersion] = config.base.match(/([^/]+)\/?$/) || [];
+  
+  const current = versions.find(({ version, aliases }) => (
+    version === currentVersion || aliases.includes(currentVersion)
+  )) || versions[0];  // Default to the first version if no match is found
 
-            var pathString = window.location.pathname;
-            if (pathString.endsWith('/')) {
-                pathString = pathString.slice(0, -1);
-            }
+  return current;
+}
 
-            currentPath = pathString.split('/');
-            currentPath.pop(); // Remove the current file
-            currentPath.push(selectedVersion);
-
-            // Reconstruct the new path
-            var newPath = '/' + currentPath.join('/');
-            window.location.pathname = newPath;
-        });
-    }
+getCurrentVersion().then(current => {
+  console.log("Current version:", current);
 });
+
+
+
+// document$.subscribe(function() {
+
+//     var versionDropdown = document.getElementById('version-select');
+//     if (versionDropdown) {
+//         // Set the dropdown to match the current URL's version on page load
+//         var currentPath = window.location.pathname.split('/');
+//         if (currentPath[0] === "") {
+//             currentPath.shift(); // Remove the leading empty element if present
+//         }
+//         var currentVersion = currentPath[currentPath.length - 2]; 
+//         versionDropdown.value = currentVersion;
+
+//         versionDropdown.addEventListener('change', function() {
+//             console.log(`callback triggered version changed to ${versionDropdown.value}`);
+//             var selectedVersion = versionDropdown.value;
+
+//             var pathString = window.location.pathname;
+//             if (pathString.endsWith('/')) {
+//                 pathString = pathString.slice(0, -1);
+//             }
+
+//             currentPath = pathString.split('/');
+//             currentPath.pop(); // Remove the current file
+//             currentPath.push(selectedVersion);
+
+//             // Reconstruct the new path
+//             var newPath = '/' + currentPath.join('/');
+//             window.location.pathname = newPath;
+//         });
+//     }
+// });
