1ac8053cc70e7eaf89ccaaac557d171c3b792423
jalinei <jalinei@users.noreply.github.com>
2025-01-20 15:14:05 +0000

    Deployed a60fa5f0 to 1.0.0 with MkDocs 1.6.1 and mike 2.1.3

--- 1.0.0/javascripts/version_selector.js
+++ 1.0.0/javascripts/version_selector.js
@@ -1,31 +1,53 @@
 document$.subscribe(function() {
-
-    var hardwareDropdown = document.getElementById('hardware-select');
-    if (hardwareDropdown) {
+    var versionButton = document.getElementById('versionButton');
+    if (versionButton) {
         // Set the dropdown to match the current URL's version on page load
         var currentPath = window.location.pathname.split('/');
-        if (currentPath[0] === "") {
-            currentPath.shift(); // Remove the leading empty element if present
+        var currentVersion = currentPath[currentPath.length - 3]; 
+        var currentBoard = currentPath[currentPath.length - 4]; 
+        var versions = {};
+        if (currentBoard == "twist") {
+            versions = {
+                "1.4.1": {
+                    buttonText: "1.4.1",
+                    aliasText: "Latest"
+                },
+                "1.3.0": {
+                    buttonText: "1.3",
+                    aliasText: "Stable"
+                },
+                "1.2.0": {
+                    buttonText: "1.2",
+                    aliasText: "Stable"
+                }
+            };
         }
-        var currentVersion = currentPath[currentPath.length - 2]; 
-        // hardwareDropdown.value = currentVersion;
-
-        hardwareDropdown.addEventListener('change', function() {
-            console.log(`callback triggered`);
-            // var selectedVersion = versionDropdown.value;
-
-            // var pathString = window.location.pathname;
-            // if (pathString.endsWith('/')) {
-            //     pathString = pathString.slice(0, -1);
-            // }
-
-            // currentPath = pathString.split('/');
-            // currentPath.pop(); // Remove the current file
-            // currentPath.push(selectedVersion);
-
-            // // Reconstruct the new path
-            // var newPath = '/' + currentPath.join('/');
-            // window.location.pathname = newPath;
-        });
+        else if (currentBoard == "spin") {
+            versions = {
+                "1.2.0": {
+                    buttonText: "1.2.0",
+                    aliasText: "Latest"
+                },
+                "1.1.0": {
+                    buttonText: "1.1.0",
+                    aliasText: "Stable"
+                },
+                "1.0.0": {
+                    buttonText: "1.0.0",
+                    aliasText: "Stable"
+                }
+            };
+        }
+        else if (currentBoard == "ownverter") {
+            versions = {
+                "1.0.0": {
+                    buttonText: "1.0.0",
+                    aliasText: "Latest"
+                }
+            };
+        }
+        if (versions[currentVersion]) {
+            versionButton.innerHTML = `${versions[currentVersion].buttonText} <span class="md-version__alias">${versions[currentVersion].aliasText}</span>`;
+        }
     }
 });
